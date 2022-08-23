e0103838375e07d8b1d1c3ac140c5839790f5141
Reinaldo <naldos15@hotmail.com>
2022-08-22 21:42:39 -0300

    relacionamento cidade e estado

---
+++ src/main/java/com/reinaldo/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.reinaldo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.reinaldo.domain.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+	
+}
