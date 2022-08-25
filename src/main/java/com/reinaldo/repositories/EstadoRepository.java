75d9ffd46ac44b34b28339bb91863f10b2a9563c
Reinaldo <naldos15@hotmail.com>
2022-08-24 22:10:44 -0300

    findAll Estado

--- src/main/java/com/reinaldo/repositories/EstadoRepository.java
+++ src/main/java/com/reinaldo/repositories/EstadoRepository.java
@@ -1,9 +1,14 @@
 package com.reinaldo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.reinaldo.domain.Estado;
 
 public interface EstadoRepository extends JpaRepository<Estado, Integer>{
 
+	@Query("SELECT obj FROM Estado obj")
+	List<Estado> findAlll();
 }
