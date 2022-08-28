d91771758177fd8b4dd867b99838b1467a60eeb4
Reinaldo <naldos15@hotmail.com>
2022-08-27 22:22:34 -0300

    repository cidade

--- src/main/java/com/reinaldo/repositories/CidadeRepository.java
+++ src/main/java/com/reinaldo/repositories/CidadeRepository.java
@@ -1,10 +1,15 @@
 package com.reinaldo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.reinaldo.domain.Cidade;
 
 public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
 
+	@Query("SELECT obj FROM Cidade obj")
+	List<Cidade> findAlll();
 	
 }
